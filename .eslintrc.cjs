3507764f9f8702f145d90a5e5e2ce3f723e58b95
Francesco Maida <francesco.maida@efem.dev>
2023-11-14 17:11:10 +0100

    Added icons rendering

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:astro/recommended'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    parserOptions: {
+        tsconfigRootDir: __dirname,
+        sourceType: 'module',
+        ecmaVersion: 'latest'
+    },
+    overrides: [
+        {
+            files: ['*.astro'],
+            parser: 'astro-eslint-parser',
+            parserOptions: {
+                parser: '@typescript-eslint/parser',
+                extraFileExtensions: ['.astro']
+            },
+            rules: {
+                // override/add rules settings here, such as:
+                // "astro/no-set-html-directive": "error"
+                "prefer-const": "error",
+            }
+        }
+    ]
+};
---
+++ .prettierrc.mjs
@@ -0,0 +1,20 @@
+/** @type {import("prettier").Config} */
+
+export default {
+    // i am just using the standard config, change if you need something else
+
+    trailingComma: "es5",
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+
+    plugins: ['prettier-plugin-astro'],
+    overrides: [
+        {
+            files: '**/*.astro',
+            options: {
+                parser: 'astro'
+            }
+        }
+    ]
+}
